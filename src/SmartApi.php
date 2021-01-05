7dbea8c4d824dfd7d7b9d8c03d913ca1ca1a0902
Hossein <hosseinyaghmaee@gmail.com>
2021-01-05 13:21:10 +0330

    create smartApi solution

---
+++ src/SmartApi.php
@@ -0,0 +1,89 @@
+<?php
+
+
+namespace Hsy\Simotel;
+
+
+class SmartApi
+{
+    private $config;
+    private $errorMessage;
+    private $response;
+
+    public function __construct(array $smartApiConfig = [])
+    {
+        $this->config = $smartApiConfig;
+    }
+
+
+    /**
+     * @param $data
+     * @return $this|bool
+     */
+    public function call($data)
+    {
+        $appName = $data['app_name'] ?? "";
+        if (!$appName)
+            return $this->fail("SmartApi data has not 'app_name' index");
+
+        $className = $this->findAppClass($appName);
+        if (!$className)
+            return false;
+
+        $class = new $className;
+        if (!method_exists($class, $appName))
+            return $this->fail("Responsible method for app name not found in '{$className}' ");
+
+        $response = $class->$appName($data);
+        if (!is_array($response))
+            return $this->fail("Returned data must be array");
+
+        $this->response = $response;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return json_encode($this->response);
+    }
+
+    public function toArray()
+    {
+        return $this->response;
+    }
+
+    /**
+     * @param $appName
+     * @return |null
+     */
+    private function findAppClass($appName)
+    {
+        if (!isset($this->config["appClasses"]))
+            return $this->fail("'appClasses' not defined in config");
+
+        $appClasses = $this->config["appClasses"];
+
+        if (isset($appClasses[$appName]))
+            return $appClasses[$appName];
+
+        if (isset($appClasses["*"]))
+            return $appClasses["*"];
+
+
+        return $this->fail("Responsible class for app name not found");
+
+    }
+
+    /**
+     * @param null $message
+     * @return bool
+     */
+    private function fail($message = null)
+    {
+        $this->errorMessage = $message;
+        return false;
+    }
+
+
+}
---
+++ src/SmartApiCommands.php
@@ -0,0 +1,129 @@
+<?php
+
+
+namespace Hsy\Simotel;
+
+
+trait SmartApiCommands
+{
+    private $apiCommands;
+
+    private function addCommand($command)
+    {
+        $this->apiCommands[] = $command;
+    }
+
+    private function render()
+    {
+        $commands = implode(";", $this->apiCommands);
+        $this->apiCommands = [];
+        return $commands;
+    }
+
+    private function okResponse()
+    {
+        return ["ok" => 1,
+            "commands" => $this->render()];
+    }
+
+    private function errorResponse()
+    {
+        return ["ok" => 0];
+    }
+
+    /*
+     *
+     */
+    private function cmdPlayAnnouncement($file)
+    {
+        $this->addCommand("PlayAnnouncement('$file')");
+    }
+
+    /*
+     *
+     */
+    private function cmdPlayback($file)
+    {
+        $this->addCommand("Playback('$file')");
+    }
+
+    /*
+     *
+     */
+    private function cmdSayDigit($number)
+    {
+        $this->addCommand("SayDigit($number)");
+    }
+
+    /*
+     *
+     */
+    private function cmdSayNumber($number)
+    {
+        $this->addCommand("SayNumber($number)");
+    }
+
+    /*
+    *
+    */
+    private function cmdSayDuration($duration)
+    {
+        $this->addCommand("SayDuration('$duration')");
+    }
+
+    /*
+     *
+     */
+    private function cmdSayClock($clock)
+    {
+        $this->addCommand("SayClock($clock)");
+    }
+
+    /*
+     *
+     */
+    private function cmdSayDate($date,$calender)
+    {
+        $this->addCommand("SayDate('$date','$calender')");
+    }
+
+    /*
+     *
+     */
+    private function cmdGetData($file,$timeout,$digitsCount)
+    {
+        $this->addCommand("GetData('$file',$timeout,$digitsCount)");
+    }
+
+    /*
+    *
+    */
+    private function cmdSetExten($exten)
+    {
+        $this->addCommand("SetExten('$exten')");
+    }
+
+    /*
+     * Set limit on call
+     */
+    private function cmdSetLimitOnCall($seconds)
+    {
+        $this->addCommand("SetLimitOnCall($seconds)");
+    }
+
+    /*
+     *
+     */
+    private function MusicOnHold()
+    {
+        //
+    }
+
+    /*
+     *
+     */
+    private function cmdExit($exit)
+    {
+        $this->addCommand("Exit('$exit')");
+    }
+}
