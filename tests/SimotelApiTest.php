fef3b7504b7bcfb10dfb13ae0106b18e92d43e0c
Hossein <hosseinyaghmaee@gmail.com>
2021-01-12 21:36:15 +0330

    Update SimotelApiTest.php

--- tests/SimotelApiTest.php
+++ tests/SimotelApiTest.php
@@ -9,17 +9,47 @@ use Hsy\Simotel\SimotelApi\Pbx\Users;
 
 class SimotelApiTest extends TestCase
 {
-    public function testAddQueue()
+    private $simotel;
+
+    public function __construct()
     {
+        $this->simotel = new Simotel();
+        parent::__construct();
+    }
+
+    public function testPbxUsers()
+    {
+        $number = "testNumber";
+
         $parameters = new Parameters;
-        $parameters->name = "test name";
-        $parameters->number = "1000";
+        $parameters->name = "test name ";
+        $parameters->number = "$number";
         $parameters->secret = "secret";
 
-        $users = new \Hsy\Simotel\SimotelApi\Pbx\Users();
-        $users->create($parameters);
+        $result = $this->simotel->connect()->pbx()->users()->create($parameters);
+        $this->assertEquals(1, $result->success);
 
-        $simotel = new Simotel();
-        $simotel->connect()->pbx()->users()->create($parameters);
+        $newName = "new name";
+
+        $parameters = new Parameters;
+        $parameters->id = "$number";
+        $parameters->name = $newName;
+
+        $result = $this->simotel->connect()->pbx()->users()->update($parameters);
+        $this->assertEquals(1, $result->success);
+
+        $parameters = new Parameters;
+        $parameters->conditions = ["number" => "$number"];
+        $parameters->alike = 1;
+
+        $result = $this->simotel->connect()->pbx()->users()->search($parameters);
+        $this->assertEquals(1, $result->success);
+        $this->assertEquals($result->data[0]->name, $newName);
+
+        $parameters = new Parameters;
+        $parameters->id = "$number";
+
+        $result = $this->simotel->connect()->pbx()->users()->remove($parameters);
+        $this->assertEquals(1, $result->success);
     }
 }
