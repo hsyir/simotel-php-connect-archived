d862e055fd9f2f34ae0f75d5147cf07400a61d5d
Hossein <hosseinyaghmaee@gmail.com>
2021-01-13 12:35:40 +0330

    Update SimotelApiTest.php

--- tests/SimotelApiTest.php
+++ tests/SimotelApiTest.php
@@ -2,10 +2,14 @@
 
 namespace Hsy\Simotel\Tests;
 
-use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
 use Hsy\Simotel\Simotel;
 use Hsy\Simotel\SimotelApi\Parameters;
-use Hsy\Simotel\SimotelApi\Pbx\Users;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\RequestException;
 
 class SimotelApiTest extends TestCase
 {
@@ -16,7 +20,7 @@ class SimotelApiTest extends TestCase
         $this->simotel = new Simotel();
         parent::__construct();
     }
-
+/*
     public function testPbxUsers()
     {
         $number = "testNumber";
@@ -51,5 +55,65 @@ class SimotelApiTest extends TestCase
 
         $result = $this->simotel->connect()->pbx()->users()->remove($parameters);
         $this->assertEquals(1, $result->success);
+    }
+*/
+
+    public function testAllMethods()
+    {
+        $namespaces = [
+            "pbx" => [
+                "Users" => ["create", "update", "search", "remove"],
+                "Trunks" => ["create", "update", "search", "remove"],
+                "Queues" => ["create", "update", "search", "remove", "addAgent", "removeAgent", "pauseAgent", "resumeAgent"],
+                "blacklists" => ["add", "update", "search", "remove"],
+                "whitelists" => ["add", "update", "search", "remove"],
+                "announcements" => ["upload", "add", "update", "search", "remove"],
+                "musicOnHolds" => ["search"],
+                "faxes" => ["upload", "add", "search", "download"],
+            ],
+            "call" => [
+                "originate" => ["act"]
+            ],
+            "voicemails" => [
+                "voicemails" => ["add", "update", "remove", "search"],
+                "inbox" => ["read", "search"],
+            ],
+            "reports" => [
+                "quick" => ["search", "info", "cdr"],
+            ],
+            "autodialer" => [
+                "announcements" => ["upload", "add", "update", "remove", "search"],
+                "campaigns" => ["add", "update", "remove", "search"],
+                "contacts" => ["add", "update", "remove", "search"],
+                "groups" => ["upload", "add", "update", "remove", "search"],
+                "reports" => ["info", "search"],
+                "trunks" => ["update", "search"],
+            ],
+        ];
+
+        foreach ($namespaces as $namespace => $groups)
+            foreach ($groups as $group => $methods)
+                array_walk($methods, $this->executeMethods($namespace, $group));
+
+    }
+
+    public function executeMethods($namespace, $group)
+    {
+        return function ($method) use ($namespace, $group) {
+            $result = $this->simotel->connect()->$namespace()->$group(null, $this->createHttpClient())->$method();
+            self::assertEquals(1, $result->success);
+        };
+    }
+
+    public function createHttpClient()
+    {
+        $res = json_encode(["success" => 1, "message" => "message", "data" => ["data"]]);
+        // Create a mock and queue two responses.
+        $mock = new MockHandler([
+            new Response(200, [], $res),
+        ]);
+
+        $handlerStack = HandlerStack::create($mock);
+        return new Client(['handler' => $handlerStack]);
     }
 }
